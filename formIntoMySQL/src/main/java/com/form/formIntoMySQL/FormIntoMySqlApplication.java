ee665a6d0dd643175d54814962c2ad37ffb80d32
Jclian <33828982+percent4@users.noreply.github.com>
2018-04-10 16:12:22 +0800

    Add files via upload

---
+++ formIntoMySQL/src/main/java/com/form/formIntoMySQL/FormIntoMySqlApplication.java
@@ -0,0 +1,12 @@
+package com.form.formIntoMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormIntoMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormIntoMySqlApplication.class, args);
+	}
+}
---
+++ formIntoMySQL/src/main/java/com/form/formIntoMySQL/entity/User.java
@@ -0,0 +1,68 @@
+package com.form.formIntoMySQL.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private String email;
+    private String city;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
